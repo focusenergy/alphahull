6ef0147818fd09642925ac3b1c7bda269b732cd0
Ken Williams <kenahoo@gmail.com>
2015-07-15 14:25:35 -0500

    Simplify & speed up inahull() by using vectorized operations

--- R/inahull.R
+++ R/inahull.R
@@ -2,56 +2,28 @@ inahull <-
 function (ahull.obj, p) 
 {
     compl <- ahull.obj$complement
-    halfpl <- which(compl[, "r"] < 0)
-    n.halfpl <- length(halfpl)
-    ball <- which(compl[, "r"] > 0)
-    n.ball <- length(ball)
-    in.compl <- FALSE
-    if (n.halfpl >= 1) {
-        h <- 1
-        while ((h <= n.halfpl) & in.compl == FALSE) {
-            sig = compl[halfpl[h], 3]
-            a = compl[halfpl[h], 1]
-            b = compl[halfpl[h], 2]
-            if (sig <= -3) {
-                if (p[1] > a) {
-                  if (sig == -3) {
-                    in.compl <- TRUE
-                  }
-                }
-                else if (p[1] < a) {
-                  if (sig == -4) {
-                    in.compl <- TRUE
-                  }
-                }
-            }
-            else {
-                if (p[2] > a + b * p[1]) {
-                  if (sig == -1) {
-                    in.compl <- TRUE
-                  }
-                }
-                else if (p[2] < a + b * p[1]) {
-                  if (sig == -2) {
-                    in.compl <- TRUE
-                  }
-                }
-            }
-            h <- h + 1
-        }
-    }
-    if (in.compl == FALSE) {
-        k <- 1
-        while ((k <= n.ball) & in.compl == FALSE) {
-            r = compl[ball[k], 3]
-            c1 = compl[ball[k], 1]
-            c2 = compl[ball[k], 2]
-            d <- sqrt((p[1] - c1)^2 + (p[2] - c2)^2)
-            if (d < r) {
-                in.compl <- TRUE
-            }
-            k <- k + 1
-        }
-    }
-    return(in.ahull = !in.compl)
+
+    ## TODO it might be better to store half-planes as simply (a,b,c) where ax+by+c>0.
+    halfp <- compl[compl[, "r"] < 0, ]
+    sig = halfp[,3]
+    a = halfp[,1]
+    b = halfp[,2]
+
+    in.halfp <-
+        (p[1] > a & sig == -3) |             # x > a;       1x + 0y - a > 0
+        (p[1] < a & sig == -4) |             # x < a;      -1x + 0y + a > 0
+        (p[2] > a + b * p[1] & sig == -1) |  # y > a + bx; -bx + 1y - a > 0
+        (p[2] < a + b * p[1] & sig == -2)    # y < a + bx;  bx - 1y + a > 0
+    if (any(in.halfp))
+        return(FALSE)
+
+    ball <- compl[compl[, "r"] > 0, ]
+    r = ball[,3]
+    c1 = ball[,1]
+    c2 = ball[,2]
+    d <- sqrt((p[1] - c1)^2 + (p[2] - c2)^2)
+    if (any(d < r))
+        return(FALSE)
+
+    return(TRUE)
 }
